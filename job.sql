5cd1a5f74892f85ea57bd2b694365329ec91501e
ntvien <62584322+ntvien@users.noreply.github.com>
2020-12-06 16:37:20 +0700

    create table Job in mysql

---
+++ job.sql
@@ -0,0 +1,11 @@
+CREATE TABLE Job(
+	job_title VARCHAR(200), 
+    company VARCHAR(200), 
+    salary VARCHAR(200), 
+    location VARCHAR(200), 
+    position VARCHAR(200), 
+    job_description VARCHAR(200), 
+    job_requirement VARCHAR(200), 
+    benefit VARCHAR(200), 
+    quantity VARCHAR(200)
+);
