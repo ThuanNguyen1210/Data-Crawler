67a76c4d4b7748aba5c3201ff3d39abea07d4a93
Bui Huu Dang <buihuudang2000@gmail.com>
2020-12-16 22:29:34 +0700

    new model

--- 1tableJob/job_mysql.py
+++ 1tableJob/job_mysql.py
@@ -8,7 +8,7 @@ con = pymysql.connect(host = "localhost", user = "root", password="", db="job")
 
 
 cursor = con.cursor()
-
+i=0
 for item in json_obj:
     job_title = item.get("job_title")
     company = item.get("company")
@@ -19,7 +19,9 @@ for item in json_obj:
     job_requirement = item.get("job_requirement")
     benefit = item.get("benefit")
     quantity = item.get("quantity")
+    i+=1
 
+    ID= "C"+str(i)
     cursor.execute(
         "insert into job(job_title, company, salary, location, position, job_description, job_requirement, benefit, quantity) values(%s, %s, %s, %s, %s, %s, %s, %s, %s)", (job_title, company, salary, location, position, job_description, job_requirement, benefit, quantity))
 con.commit()
