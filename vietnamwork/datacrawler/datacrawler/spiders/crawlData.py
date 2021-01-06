fada64a168ec6dd4d75133c8eeccfdd81ed99d33
Thuan Nguyen <thuannguyen121000@gmail.com>
2021-01-06 14:39:56 +0700

    Fix format of datafile, fresh the data

--- vietnamwork/datacrawler/datacrawler/spiders/crawlData.py
+++ vietnamwork/datacrawler/datacrawler/spiders/crawlData.py
@@ -20,22 +20,22 @@ class DataSpider(scrapy.Spider):
     def parse(self, response):
         global i
         yield {
-            'job-title': response.css('div.job-header-info h1.job-title::text').get().strip(),
+            'job_title': response.css('div.job-header-info h1.job-title::text').get().strip(),
             'company': response.css('div.job-header-info div.row a::text').get().strip(),
             'salary': salaries[i].strip(),
-            'location': list(filter(None, ([response.css('div.location div.location-name::text')[i].get().strip('+-–*o"•·\\>■\n\r\t ') \
+            'location': str(list(filter(None, ([response.css('div.location div.location-name::text')[i].get().strip('+-–*o"•·●\\>■✅\n\r\"\\ ').replace('\t', '') \
                             for i in range(len(response.css('div.location div.location-name::text').getall()))
-                            ]))),
-            'position': response.css('div.job-header-info h1.job-title::text').get().strip(),
-            'job_description': list(filter(None, ([response.css('div.description::text')[i].get().strip('+-–*o"•·\\>■\n\r\t ') \
+                            ])))).replace('[', '').replace(']', '').replace("'", ""),
+            'position': response.css('div.job-header-info h1.job-title::text').get().strip().replace('\t', ''),
+            'job_description': str(list(filter(None, ([response.css('div.description::text')[i].get().strip('+-–*o"•·●\\>■✅\n\r\"\\ ').replace('\t', '') \
                             for i in range(len(response.css('div.description::text').getall()))
-                            ]))),
-            'job_requirement': list(filter(None, ([response.css('div.requirements::text')[i].get().strip('+-–*o"•·\\>■\n\r\t ') \
+                            ])))).replace('[', '').replace(']', '').replace("'", ""),
+            'job_requirement': str(list(filter(None, ([response.css('div.requirements::text')[i].get().strip('+-–*o"•·●\\>■✅\n\r\"\\ ').replace('\t', '') \
                                 for i in range(len(response.css('div.requirements::text').getall()))
-                            ]))),
-            'benefit': list(filter(None,([response.css('div.benefits div.benefit-name::text')[i].get().strip('+-–*o"•·\\>■\n\r\t ') \
+                            ])))).replace('[', '').replace(']', '').replace("'", ""),
+            'benefit': str(list(filter(None,([response.css('div.benefits div.benefit-name::text')[i].get().strip('+-–*o"•·●\\>■✅\n\r\"\\ ').replace('\t', '') \
                             for i in range(len(response.css('div.benefits div.benefit-name::text').getall()))
-                        ]))),
+                        ])))).replace('[', '').replace(']', '').replace("'", ""),
             'quantity': "1"
         }
         i += 1
