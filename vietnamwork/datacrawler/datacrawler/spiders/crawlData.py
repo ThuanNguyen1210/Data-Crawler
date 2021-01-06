9a6a27f26868aa91fd3f97b8cb302177b512c2af
Thuan Nguyen <thuannguyen121000@gmail.com>
2021-01-06 13:53:05 +0700

    Crawl data from vietnamwork.com, total 6629 jobs

--- vietnamwork/datacrawler/datacrawler/spiders/crawlData.py
+++ vietnamwork/datacrawler/datacrawler/spiders/crawlData.py
@@ -6,26 +6,36 @@ file = open('joblinks.txt', 'r')
 links = file.readlines()
 for link in links:
     urls += [link]
+
+salaryfile = open('salary.txt', 'r', encoding='utf-8')
+salaries_info = salaryfile.readlines()
+salaries = []
+for salary in salaries_info:
+    salaries += [salary]
+i = 0
 class DataSpider(scrapy.Spider):
     name = 'vietnamwork'
 
     start_urls = urls
-
     def parse(self, response):
+        global i
         yield {
             'job-title': response.css('div.job-header-info h1.job-title::text').get().strip(),
             'company': response.css('div.job-header-info div.row a::text').get().strip(),
-            'expired_time': response.css('div.job-header-info div.row')[2].css('div.col-sm-12 span.expiry.gray-light::text').get().strip(),
-            'benefits': list(filter(None,([response.css('div.benefits div.benefit-name::text')[i].get().strip() \
-                            for i in range(len(response.css('div.benefits div.benefit-name::text').getall()))
-                        ]))),
-            'description': list(filter(None, ([response.css('div.description::text')[i].get().strip() \
+            'salary': salaries[i].strip(),
+            'location': list(filter(None, ([response.css('div.location div.location-name::text')[i].get().strip('+-–*o"•·\\>■\n\r\t ') \
+                            for i in range(len(response.css('div.location div.location-name::text').getall()))
+                            ]))),
+            'position': response.css('div.job-header-info h1.job-title::text').get().strip(),
+            'job_description': list(filter(None, ([response.css('div.description::text')[i].get().strip('+-–*o"•·\\>■\n\r\t ') \
                             for i in range(len(response.css('div.description::text').getall()))
                             ]))),
-            'requirements': list(filter(None, ([response.css('div.requirements::text')[i].get().strip() \
+            'job_requirement': list(filter(None, ([response.css('div.requirements::text')[i].get().strip('+-–*o"•·\\>■\n\r\t ') \
                                 for i in range(len(response.css('div.requirements::text').getall()))
                             ]))),
-            'locations': list(filter(None, ([response.css('div.location div.location-name::text')[i].get().strip() \
-                            for i in range(len(response.css('div.location div.location-name::text').getall()))
-                            ])))
+            'benefit': list(filter(None,([response.css('div.benefits div.benefit-name::text')[i].get().strip('+-–*o"•·\\>■\n\r\t ') \
+                            for i in range(len(response.css('div.benefits div.benefit-name::text').getall()))
+                        ]))),
+            'quantity': "1"
         }
+        i += 1
